6cbdc213d87cf179112279570a6038ee37afd166
Kristen Cadacio <56667023+cadakris@users.noreply.github.com>
2022-06-18 16:22:55 -0700

    Time: 106 ms (93.20%), Space: 47.2 MB (80.34%) - LeetHub

--- 989-add-to-array-form-of-integer/989-add-to-array-form-of-integer.js
+++ 989-add-to-array-form-of-integer/989-add-to-array-form-of-integer.js
@@ -1,11 +1,4 @@
-/**
- * @param {number[]} num
- * @param {number} k
- * @return {number[]}
- */
+
 var addToArrayForm = function(num, k) {
-  let number = num.join("")
-  number = BigInt(number)
-  number += BigInt(k)
-  return(number.toString().split(""))
+  return (BigInt(num.join('')) + BigInt(k)).toString().split('')
 };
