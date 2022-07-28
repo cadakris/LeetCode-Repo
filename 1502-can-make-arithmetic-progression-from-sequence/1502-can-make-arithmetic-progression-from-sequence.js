8ee20b70adcb20a31f066eee3d7877a2f26c4250
Kristen Cadacio <56667023+cadakris@users.noreply.github.com>
2022-07-27 22:00:48 -0400

    Time: 95 ms (47.65%), Space: 42.4 MB (46.75%) - LeetHub

---
+++ 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var canMakeArithmeticProgression = function(arr) {
+        arr.sort((a,b) => a-b)
+    let diff = Math.abs(arr[0] - arr[1])
+    for (let i = 0; i < arr.length - 1;i ++) {
+        if (diff !== Math.abs(arr[i] - arr[i+1])) {
+            return false
+        }
+    }
+    
+    return true
+};
