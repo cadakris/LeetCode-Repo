02a18cc2e9877919437eeac3d118ee7ad508ef89
Kristen Cadacio <56667023+cadakris@users.noreply.github.com>
2022-08-18 11:33:21 -0400

    Time: 109 ms (81.59%), Space: 46.9 MB (30.22%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.js
+++ 283-move-zeroes/283-move-zeroes.js
@@ -10,7 +10,6 @@ function moveZeroes (nums) {
   }
   return(nums)
 }
-moveZeroes([1,3,12,0,0,5,6,7])
 
 //Want to loop through and splice the zeroes and then push a zero
 //Use count in order to keep the length of nums in the array. 
